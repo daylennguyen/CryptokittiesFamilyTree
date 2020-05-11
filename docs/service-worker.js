8992ba8d3c685e6b2a7e992a6e7e256cda7e1f6b
flannyan <daylennguyen@gmail.com>
2020-05-11 16:12:55 -0700

    adding manifest and ico

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3958d439d4d4da7da67a0eead9a3a6dc.js"
+  "/daylennguyen/CryptokittiesFamilyTreeprecache-manifest.af50b8809878d8c55c825fc54f4188b6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/daylennguyen/CryptokittiesFamilyTreeindex.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
