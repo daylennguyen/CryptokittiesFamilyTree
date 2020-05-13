c9c099687a6336b857489bd1acbe92380fe990ea
flannyan <daylennguyen@gmail.com>
2020-05-13 11:43:56 -0700

    Updates

---
+++ precache-manifest.b4658d419ea1e4821b7284d94cf49c39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa299080c81dee1526238b4ba3d2fe17",
+    "url": "/CryptokittiesFamilyTree/index.html"
+  },
+  {
+    "revision": "3432b9ccc9c751c13c53",
+    "url": "/CryptokittiesFamilyTree/static/css/main.87ce9e96.chunk.css"
+  },
+  {
+    "revision": "fcf3dfbbfc6af66a03ab",
+    "url": "/CryptokittiesFamilyTree/static/js/2.06e99503.chunk.js"
+  },
+  {
+    "revision": "f35b66177d15e93c9c5bf4166f50a6f4",
+    "url": "/CryptokittiesFamilyTree/static/js/2.06e99503.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3432b9ccc9c751c13c53",
+    "url": "/CryptokittiesFamilyTree/static/js/main.46a94418.chunk.js"
+  },
+  {
+    "revision": "bbab291d47845208a6a2",
+    "url": "/CryptokittiesFamilyTree/static/js/runtime-main.95c5ae39.js"
+  }
+]);
